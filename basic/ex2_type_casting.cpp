de565f2e5ef68c77f4c0f65f8f4dca5ca1ff44ee
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-05-13 22:51:50 +0530

    13-05-2025 Tue

---
+++ basic/ex2_type_casting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+	
+int main()
+{
+	char ch;
+	cout << "Enter char : "<<endl;
+	cin >> ch;
+	cout << "character : "<< ch << endl;
+	cout << "ASCII : "<< (int)ch << endl;
+}
