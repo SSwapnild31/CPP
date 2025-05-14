ed15b437e7c53b1886553e9889b8785349004ae8
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-05-14 18:35:57 +0530

    14-05-2025 wed

---
+++ class_assgnmt/ex3_read_ascii_and_print_char.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout <<"Enter ascii value : "<< endl;
+	cin >>x;
+	
+	cout <<"ascii char : "<< (char)x << endl;
+	
+	return 0;
+}
