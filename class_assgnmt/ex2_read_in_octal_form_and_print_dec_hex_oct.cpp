ca190b9d639f76f9be82893bfa61803ad14868df
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-05-14 18:35:42 +0530

    14-05-2025 wed

---
+++ class_assgnmt/ex2_read_in_octal_form_and_print_dec_hex_oct.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << showbase << uppercase << showpos;
+	cout <<"Enter oct num : "<< endl;
+	cin >> oct >> x;
+	
+	cout <<"dec:"<< dec << x <<" hex:"<< hex << x <<" oct:"<< oct << x << endl;
+	
+	return 0;
+}
