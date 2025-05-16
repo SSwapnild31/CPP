62325a9de78c6bec164897bfa8d31768fa858f0c
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-05-16 21:50:46 +0530

    16-05-2025 Fri

---
+++ dma/ex5_array_initialization_dma.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int *p;
+	p=new int[5]{10,20,30,40,50};
+	
+	for(int i=0;i<5;i++)
+		cout << p[i] <<" ";
+	cout << endl;
+	delete []p;
+}
