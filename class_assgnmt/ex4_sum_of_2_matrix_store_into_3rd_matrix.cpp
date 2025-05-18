b59ad6d2e888cae1c8db6a846cdfdd8f21a3a111
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-05-18 22:26:01 +0530

    18-05-2025 Sun

---
+++ class_assgnmt/ex4_sum_of_2_matrix_store_into_3rd_matrix.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+void inputMatrix(int, int, int **p);
+void printMatrix(int, int, int **p);
+void addMatrix(int, int, int **, int **, int **);
+void printMatrix(int, int, int **);
+int main()
+{
+	int r,c,i,j;
+
+	cout <<"Enter row & column"<< endl;
+	cin >> r >> c ;
+	
+	int **p,**q,**s;
+	
+	p=new int* [r];
+	q=new int* [r];
+	s=new int* [r];
+	
+	for(i=0;i<c;i++)
+	{
+		p[i]=new int;
+		q[i]=new int;
+		s[i]=new int;
+	}	
+	
+	inputMatrix(r,c,p);
+	inputMatrix(r,c,q);
+	
+	addMatrix(r,c,p,q,s);
+	
+	cout <<"sum of 2 matrix : "<< endl ;
+	printMatrix(r,c,s);
+	for(i=0;i<c;i++)
+	{
+		delete p[i];
+		delete q[i];
+	}
+	delete [] p;
+	delete [] q;
+	
+	return 0;
+}
+
+void addMatrix(int r, int c, int **p, int **q, int **s)
+{
+	for(int i=0;i<r;i++)
+		for(int j=0;j<c;j++)
+			s[i][j]=p[i][j]+q[i][j];
+}
+
+void printMatrix(int r, int c, int **p)
+{
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+			cout << p[i][j] <<" ";
+		cout << endl;
+	}	
+}
+
+void inputMatrix(int r, int c, int **p)
+{
+	cout <<"enter matrix ele : " << endl;
+	for(int i=0;i<r;i++)
+		for(int j=0;j<c;j++)
+			cin >> p[i][j];
+}
