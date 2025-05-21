6eeb28d66c7eef34e1cb61cbeb35012c910b0890
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-05-21 21:19:42 +0530

    21-05-2025 wed

---
+++ datatypes/ex7_string_concatination.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string s1 ,s2;
+	cin >> s1 >> s2;
+	
+	//cout << s1+s2 << endl;
+	
+	string res=s1+s2;
+	cout << res << endl;
+	
+	return 0;
+}
---
+++ datatypes/ex8_string_comparison.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string s1="vector",s2="indian";		
+	
+	bool res = s1 > s2;
+	cout << res << endl;
+	
+	return 0;
+}
