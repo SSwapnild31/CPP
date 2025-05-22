05c69355b94ae278d2dc4c9da1073d54e1c5afae
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-05-22 22:30:32 +0530

    22-05-2025 Thus

---
+++ oop/ex6_fun_overloading.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b)
+{
+	return a+b;
+}
+
+int sum(int a,int b,int c)
+{
+	return a+b+c;
+}
+int main()
+{
+	cout << sum(10,20) << endl;;
+	cout << sum(10,20,30) << endl;
+	return 0;
+}
