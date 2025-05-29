932bfb76e44d0f65b6ba61a115576a05663cc4bf
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-05-29 20:20:03 +0530

    29-05-2025 Thu

---
+++ oop/ex20_mutable_variable.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class family
+{
+	mutable int cash;
+	int gold;
+public :
+	void set_data()
+	{
+		cout <<"Enter cash & gold"<< endl;
+		cin >> cash >> gold;
+	}
+	void get_data()const
+	{
+		cash = 100; 
+		//gold = 200;		   //error, 'cause of const method
+		cout << cash <<" "<< gold << endl;
+	} 
+};
+
+int main()
+{
+	family f;
+	f.set_data();
+	f.get_data();
+	return 0;
+}
