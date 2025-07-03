466f8029aad54ad4fbeb4cf00fc8e404595ea56f
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-07-03 22:34:36 +0530

    03-07-2025 Thu

---
+++ files/ex1_open_and_write.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+	ofstream fout("data");
+	fout << "swapnil natha divate" << endl;
+	fout << "gurupimpri, India" << endl;
+
+	return 0;
+}
