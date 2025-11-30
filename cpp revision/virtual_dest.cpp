efeeb4cfd877f7364ad1a1136eced804428f0c84
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-11-30 16:58:04 +0530

    30-11-2025 Sun

---
+++ cpp revision/virtual_dest.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class base 
+{
+public :
+	base()
+	{
+		cout <<"base constructor"<< endl;
+	}
+	virtual ~base()
+	{
+		cout <<"base destructor"<< endl;
+	}
+};
+
+class derived : public base
+{
+public : 
+	derived()
+	{
+		cout <<"derived constructor"<< endl;
+	}
+	~derived()
+	{
+		cout <<"derived destructor"<< endl;
+	}
+};
+
+int main()
+{
+	//base b;
+	//derived d;
+
+	base *ptr = new derived;
+
+	delete ptr;
+	return 0;
+}
