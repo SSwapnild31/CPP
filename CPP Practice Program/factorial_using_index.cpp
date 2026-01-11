f8995611322e0a97e4dee78d0dd3e28d255f9265
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-01-11 21:29:52 +0530

    11-01-2026 Sun

---
+++ CPP Practice Program/factorial_using_index.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int fact(int num)
+{
+	int res = 1;
+
+	for(int i = 2; i <= num; i++){
+		res = res * i;
+	}
+	return res;
+}
+
+int main()
+{
+	int num;
+	cout <<"Enter number : ";
+	cin >> num;
+	
+	cout << fact(num) << endl;
+
+	return 0;
+}
