e1c54e60f75a602793796a43a98bcc0f3821e5be
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-01-18 23:35:01 +0530

    18-01-2026 Sun

---
+++ CPP Practice Program/substr.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	string s,m;
+	cout <<"Enter main string : ";
+	getline(cin,m);
+	cout <<"Enter substring : ";
+	getline(cin,s);
+
+	return 0;
+}
