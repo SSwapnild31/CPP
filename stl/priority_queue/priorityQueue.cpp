469fc3fb55c18bb74e59410528b69fe29d21437a
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-01-31 23:30:45 +0530

    31-01-2026 Sat

---
+++ stl/priority_queue/priorityQueue.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+	priority_queue<int> pq;
+
+	pq.push(10);
+	pq.push(30);
+	pq.push(20);
+
+	while(!pq.empty()){
+		cout << pq.top() <<" ";
+		pq.pop();
+	}
+	cout << endl;
+	
+	return 0;
+}
