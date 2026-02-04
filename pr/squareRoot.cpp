aabaa0688e573c9bde18028faca819303df68a07
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-02-04 23:41:48 +0530

    04-02-2025 Wed

---
+++ pr/squareRoot.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class solution
+{
+public:
+	int squareRoot(int num){
+		int sroot;
+		
+		
+		return sroot;
+	}
+};
+
+
+int main()
+{
+	int num;
+	cout <<"Enter square of number : ";
+	cin >> num;
+	
+	solution s;
+	cout <<"square root of "<< num <<" : "<< s.squareRoot(num) << endl;
+
+	return 0;
+}
