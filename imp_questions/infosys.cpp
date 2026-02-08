c45aeece5fa60b07ece9958e9e8733809c14c407
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-02-08 22:11:50 +0530

    08-02-2026 Sun

--- imp_questions/infosys.cpp
+++ imp_questions/infosys.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int func(int &a, int n){
+	
+	int freq[256] = {0};
+	int count = 0, lst_freq = 0;
+
+	for(int i=0; i<n; i++){
+		freq[(unsigned int)a[i]]++;
+
+		if(freq[(unsigned int)a[i]] != lst_freq){
+			count++;
+			lst_freq = freq[(unsigned int)a[i]];
+		}
+	}
+	return count;
+}
+
 int main()
 {
 	int n;
@@ -13,7 +29,7 @@ int main()
 		cin >> a[i];
 	}
 	
-	
+	cout <<" "<< func(a,n) << endl;
 
 	return 0;
 }
