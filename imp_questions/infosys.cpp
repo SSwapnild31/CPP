8fd07993616ae94da067c61f0448e6c363f57d76
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-02-08 21:52:02 +0530

    08-02-2026 Sun

---
+++ imp_questions/infosys.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout <<"Enter n : ";
+	cin >> n;
+	
+	int a[n];
+	cout <<"Enter arr ele's"<< endl;
+	for(int i=0;i<n;i++){
+		cin >> a[i];
+	}
+	
+	
+
+	return 0;
+}
