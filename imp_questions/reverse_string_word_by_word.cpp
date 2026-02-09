da48f890d93d12be1642a5c7a23d29509ebb56c6
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-02-09 22:25:20 +0530

    09-02-2026 Mon

---
+++ imp_questions/reverse_string_word_by_word.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+void strReverse(char *p, char *q){
+	while(p<q){
+	    char t = *p;
+		*p = *q;
+		*q = t;
+		p++;
+		q--;
+	}
+}
+
+int main()
+{
+	//string s;
+	//getline(cin, s);
+	
+	char s[50];
+	cout <<"Enter string : ";
+	cin.getline(s, sizeof(s));
+	
+	strReverse(s, s + (strlen(s)-1));
+	
+	int i = 0, j = 0; 
+	
+	while(s[j] != 0){
+		if(s[j] == ' '){
+			strReverse(&s[i], &s[j-1]);
+			i = j + 1;
+		}
+		else if(s[j+1] == '\0'){
+			strReverse(&s[i], &s[j]);
+		}
+		j++;
+	}
+
+	cout << s << endl;
+	
+	return 0;
+}
