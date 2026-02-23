4ca472540689736050e6d5778343985a3f2b6318
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-02-23 22:16:31 +0530

    Fix formatting issues in AbstractClass1.cpp

--- CPP Practice Program/AbstractClass1.cpp
+++ CPP Practice Program/AbstractClass1.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
 using namespace std;
 
-class Base
-{
+class Base {
 	public :
 		virtual void show()=0; //Pure virtual function
 };
 
-class Derived : public Base
-{
+class Derived : public Base {
 	public :
 		void show()
 		{
@@ -24,3 +22,4 @@ int main()
 	b->show();
 	return 0;
 }
+
