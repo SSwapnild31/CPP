88e016a4666bcbcf69d9b961e0674c2c11ed691c
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-03-01 23:21:21 +0530

    01-03-2026 Sun

--- CPP Practice Program/unordered_map.cpp
+++ CPP Practice Program/unordered_map.cpp
@@ -1,15 +1,13 @@
 #include<iostream>
 #include<unordered_map>
-#include<map>
 using namespace std;
 
 int main()
 {
-	//map<string, int> ump;
 	unordered_map<string, int> ump;
 	
 	ump["blr "] = 75;
-	ump["pune"] = 95;
+	ump["pune"] = 85;
 	ump["mum "] = 92;
 	
 	for(auto &i : ump){
