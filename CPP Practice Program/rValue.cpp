fc2595ae6ea37bd38ac291edbd06f387ce2b609d
Swapnil Divate <swapnil.nd7057@gmail.com>
2026-03-10 00:06:34 +0530

    10-03-2026 Mon

--- CPP Practice Program/rValue.cpp
+++ CPP Practice Program/rValue.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
 
-void printName(std::string& name){
+void printName(const std::string& name){
+	std::cout <<"[lvalue] "<< name << std::endl;
+}
 
-	std::cout << name << std::endl;
+void printName(std::string&& name){
+	std::cout <<"[rvalue] "<< name << std::endl;
 }
 
 int main()
 {
 	std::string fname = "Swapnil";
-	std::string lname = " Divate";
+	std::string lname = "Divate";
 
 	std::string fullname = fname + lname;
 
 	printName(fullname);
 	printName(fname + lname);
-
 }
